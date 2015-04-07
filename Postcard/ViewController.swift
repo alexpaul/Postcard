80b29bd796ccfaae138bd335e2809efe75b62614
Alex Paul <axp_alpaul@yahoo.com>
2015-04-07 06:04:09 -0400

    Challenge 1

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,22 +12,40 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
 
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Tenary Operator
         //self.messageLabel.hidden = (self.messageLabel.hidden) ? false : true
-        // Adding a Comment to Test GitHub Commits 
+        
         self.messageLabel.hidden = false
-        self.messageLabel.text = self.enterMessageTextField.text
+        self.messageLabel.text = "Message: " + self.enterMessageTextField.text
         self.messageLabel.textColor = UIColor.redColor()
         
         self.enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
-        self.mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        if(self.messageLabel.text == ""){ // empty
+            self.mailButton.setTitle("Send Mail", forState: UIControlState.Normal)
+        }else{ // NOT empty
+            self.mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        }
+        
+        // unhide the nameLabel
+        self.nameLabel.hidden = false
+        
+        // update the nameLabel's Text to display the information in the enterNameTextField
+        self.nameLabel.text = "Name: " + self.enterNameTextField.text
+        
+        // change the nameLabel's textColor to blue 
+        self.nameLabel.textColor = UIColor.blueColor()
+        
+        // clear the enterNameTextField text 
+        self.enterNameTextField.text = ""
     }
 
 }
